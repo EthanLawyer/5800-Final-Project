e17e7e44897ddb2f9df878fc3d7824694774cf1f
EthanLawyer <zhu.yixiao@outlook.com>
2023-12-07 00:24:38 -0800

    Simplified the grab_data function to directly convert the .csv in the url into a pandas dataframe.

--- data_processing/grab_data.py
+++ data_processing/grab_data.py
@@ -1,18 +1,18 @@
 '''
-Grabs the csv data from the url and returns it as a string
+Grabs the csv data from the url and returns it as a pandas dataframe.
 Author: Yixiao Zhu
 '''
 
-import requests
+import pandas as pd
 KEY_WORD = "http"
 
 def grab_data(url):
     '''
-    Function: to retrieve the csv file from inputted url, and return it as a uncleaned string
+    Function: to retrieve the csv file from inputted url, and return it as a uncleaned pandas dataframe.
     Parameters:
         url -- a string
     Returns:
-        text -- a string of the uncleaned csv data
+        raw_df -- a pandas dataframe, the uncleaned dataframe of the csv file
     Raises:
         TypeError -- if the input is not a str, or if required parameters are not inputted
         ValueError -- if the inputted str is not a url
@@ -21,6 +21,8 @@ def grab_data(url):
         Timeout -- when connection timeouts
         TooManyRedirects -- when failed to connnect after too many redirects
         RequestException -- when other error occurs
+        ParserError -- when parser error occurs
+        EmptyDataError -- when empty data error occurs
     '''    
     if url is None:
         raise TypeError("Error, function 'grab_data' requires parameter 'url'.")
@@ -33,13 +35,8 @@ def grab_data(url):
 
     else:
         try:
-            response = requests.get(url)
-
-            if response.status_code == 200:               
-                text = response.text
-                return text
-            else:
-                response.raise_for_status()
+            raw_df = pd.read_csv(url, sep=";")
+            return raw_df
 
         except requests.exceptions.HTTPError as http_error:
             raise requests.exceptions.HTTPError(f"HTTPError.{http_error}")
@@ -51,5 +48,11 @@ def grab_data(url):
             raise requests.exceptions.TooManyRedirects(f"Error. TooManyRedirects. {red_error}")
         except requests.exceptions.RequestException as req_error:
             raise requests.exceptions.RequestException(f"Error occured.{req_error}")
+        except pd.errors.ParserError as parser_error:
+            raise pd.errors.ParserError(f"Error. {parser_error}")
+        except pd.errors.EmptyDataError as empty_error:
+            raise pd.errors.EmptyDataError(f"Error. {empty_error}")
+        except Exception as exception:
+            raise Exception(f"Error. {exception}")
 
 
